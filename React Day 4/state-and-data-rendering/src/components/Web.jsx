46e26fe23c92fbf88c76a0b14dabb53db3319b21
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-08-08 03:12:08 +0530

    Better Approach  of Handling the Forms

--- React Day 4/state-and-data-rendering/src/components/Web.jsx
+++ React Day 4/state-and-data-rendering/src/components/Web.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 
 const Web =()=>{
-     let [name, setName] = useState("");
-     let [password, setPassword] = useState("");
-     let [email, setEmail] = useState("");
+    let [formData, setFormData] = useState({
+        name : "",
+        email : "",
+        password : ""
+    });
+
+
     return(
         <div className="flex flex-col gap-5">
             <input onChange={(e)=>{
-              setName(e.target.value);
-            }} className="p-2 border-2 rounded w-60" type="text"placeholder="Name" />
+              setFormData({ ...formData, name: e.target.value });
+            }} className="p-2 border-2 rounded w-60" type="text" placeholder="Name" />
             <input onChange={(e)=>{
-                setEmail(e.target.value);
+                setFormData({ ...formData, email: e.target.value });
             }} className="p-2 border-2 rounded w-60" type="text" name="" placeholder="Email" />
             <input onChange={(e)=>{
-                setPassword(e.target.value);
+                setFormData({ ...formData, password: e.target.value });
             }} className="p-2 border-2 rounded w-60" type="text" name="" placeholder="Password" />
             <button className="p-2 border-2 rounded w-20" >Submit</button>
 
-            <h1>Name is {name}</h1>
-            <h1>Email is {email}</h1>
-            <h1>Password is {password}</h1>
+            <h1>Your name is {formData.name}</h1>
         </div>
     )
 }
