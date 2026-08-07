9771e60209403ea8a9fd8ebb03a9c028ec3a6231
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-08-07 19:52:37 +0530

    Practised the Statefunction and Data rendering

---
+++ React Day 4/state-and-data-rendering/src/Counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Counter = () =>{  
+  
+    let [count, setCount] = useState(0);
+    return (
+     <div>
+        <h1>Count is {count}</h1>
+        <button onClick={()=>{
+           setCount(count+1);
+        }}>Increase</button>
+     </div>
+    );
+};
+
+export default Counter;
