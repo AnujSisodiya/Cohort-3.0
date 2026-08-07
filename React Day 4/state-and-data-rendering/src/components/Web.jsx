5aec24e333766932aa5b717760f560bf130c9ab6
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-08-08 03:19:18 +0530

    Optimised  Approach  of Handling the Forms where we target the imputs with evnets and name type in the inputs

--- React Day 4/state-and-data-rendering/src/components/Web.jsx
+++ React Day 4/state-and-data-rendering/src/components/Web.jsx
@@ -7,18 +7,22 @@ const Web =()=>{
         password : ""
     });
 
+  const handleChange = (e) =>{
+    let {name ,value} = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
 
     return(
         <div className="flex flex-col gap-5">
-            <input onChange={(e)=>{
-              setFormData({ ...formData, name: e.target.value });
-            }} className="p-2 border-2 rounded w-60" type="text" placeholder="Name" />
-            <input onChange={(e)=>{
-                setFormData({ ...formData, email: e.target.value });
-            }} className="p-2 border-2 rounded w-60" type="text" name="" placeholder="Email" />
-            <input onChange={(e)=>{
-                setFormData({ ...formData, password: e.target.value });
-            }} className="p-2 border-2 rounded w-60" type="text" name="" placeholder="Password" />
+            <input onChange={
+              handleChange
+            } className="p-2 border-2 rounded w-60" type="text"name="name" placeholder="Name" />
+            <input onChange={
+              handleChange
+            } className="p-2 border-2 rounded w-60" type="text" name="email" placeholder="Email" />
+            <input onChange={
+             handleChange
+            } className="p-2 border-2 rounded w-60" type="text" name="password" placeholder="Password" />
             <button className="p-2 border-2 rounded w-20" >Submit</button>
 
             <h1>Your name is {formData.name}</h1>
