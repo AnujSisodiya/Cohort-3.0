54baab7d01dceecc8a289cd96670855ed25c8329
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-08-10 01:36:20 +0530

    Practised useRef

--- UseRef_RHU/src/components/Form.jsx
+++ UseRef_RHU/src/components/Form.jsx
@@ -1,31 +1,70 @@
-import React from 'react'
-import {useForm} from 'react-hook-form'
+import React from "react";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-  
-  let {handleSubmit,register,reset, formState:{errors}} = useForm();
+  let [formData, setFromdata] = useState({});
+  const formRef = useRef({});
 
-  const formSubmit = (data) =>{
-    console.log(data);
-    reset();
-  }
+  const handleSumbit = (e) => {
+    e.preventDefault();
+
+    let obj = {
+      productName: formRef.current.productName.value,
+      price: formRef.current.price.value,
+      category: formRef.current.category.value,
+      image: formRef.current.image.value,
+    };
+
+    setFromdata(obj);
+  };
   return (
-    <div className='p-4 w-80 '>
-        <form onSubmit={handleSubmit(formSubmit)} className=' bg-white flex flex-col p-6 gap-5 rounded-xl'>
-            <input {...register("ProductName")} className='border-2 p-2 rounded' type="text" placeholder='Product Name' />
-            <input {...register("Price")} className='border-2 p-2 rounded' type="text" placeholder='Price' />
-            <span>Select Category:</span>
-            <select {...register("Category")} className='border-2 p-2 rounded' name="" id="">
-                <option value="">Select</option>
-                <option value="">Men</option>
-                <option value="">Women</option>
-                <option value="">Kids</option>
-            </select>
-            <input {...register("Image")} className='border-2 p-2' type="text" placeholder='Image Url`' />
-            <button className='p-2 bg-blue-500 text-white rounded uppercase'>Create</button>
-        </form>
-    </div>
-  )
-}
+    <div className="p-4 w-80 ">
+      <form
+        onSubmit={handleSumbit}
+        className=" bg-white flex flex-col p-6 gap-5 rounded-xl"
+      >
+        <input
+          ref={(e) => (formRef.current.productName = e)}
+          className="border-2 p-2 rounded"
+          type="text"
+          placeholder="Product Name"
+        />
+        <input
+          ref={(e) => (formRef.current.price = e)}
+          className="border-2 p-2 rounded"
+          type="text"
+          placeholder="Price"
+        />
+        <span>Select Category:</span>
+        <select
+          ref={(e) => (formRef.current.category = e)}
+          className="border-2 p-2 rounded"
+          name=""
+          id=""
+        >
+          <option value="">Select</option>
+          <option value="">Men</option>
+          <option value="">Women</option>
+          <option value="">Kids</option>
+        </select>
+        <input
+          ref={(e) => (formRef.current.image = e)}
+          className="border-2 p-2"
+          type="text"
+          placeholder="Image Url"
+        />
+        <button className="p-2 bg-blue-500 text-white rounded uppercase">
+          Create
+        </button>
+      </form>
 
-export default Form
+      <h1>{formData.productName}</h1>
+      <h1>{formData.price}</h1>
+      <h1>{formData.category}</h1>
+      <h1>{formData.image}</h1>
+    </div>
+  );
+};
+
+export default Form;
