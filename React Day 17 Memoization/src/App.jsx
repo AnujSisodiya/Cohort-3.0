6b2ea719124e8e5a06ae84e9b38b299c5a29bef5
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-08-28 08:22:10 +0530

    Effect of prop drilling on react memo

--- React Day 17 Memoization/src/App.jsx
+++ React Day 17 Memoization/src/App.jsx
@@ -6,12 +6,18 @@ import About from './Components/About';
 const App = () => {
   console.log('App Rendering');
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState({ name: 'Raghav', id: 234 });
   return (
     <div>
       <h1>Memoization</h1>
       <h2>Count is {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <Home />
+      <h1>Name is {user.name}</h1>
+      <button onClick={() => setUser({ ...user, name: 'Anuj' })}>
+        Change Name
+      </button>
+      <Home user={user} />
+      {/* Receives 'user' object prop. React.memo does shallow comparison on user reference. Here The value is keep changing so Home will re-render */}
       <About />
     </div>
   );
