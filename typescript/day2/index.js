640594ce0c7626609f700b56b998b18dc82515fd
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-09-22 04:15:23 +0530

    Type Inference and Annotation

---
+++ typescript/day2/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//TYPE INFERENCE AND  ANNOTATION
+let a = 90;
+a = 20;
+a = 'pil';
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ typescript/day2/index.ts
@@ -0,0 +1,6 @@
+//TYPE INFERENCE AND  ANNOTATION
+let a = 90;
+a = 20;
+a = 'pil';
+
+console.log(a);
