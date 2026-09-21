f7e2b0b9e4924fb22732e728cc7cd000f62060d6
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-09-22 04:20:26 +0530

    Primitive Data Types

--- typescript/day2/index.js
+++ typescript/day2/index.js
@@ -1,8 +1,22 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //TYPE INFERENCE AND  ANNOTATION
+Object.defineProperty(exports, "__esModule", { value: true });
+//Inference is the type of data passed to the variable first time it assign the data type same as data automatically
+// Where as Annotation is the Data type is defined by us.
 let a = 90;
 a = 20;
-a = 'pil';
 console.log(a);
+//Primitive Data Types
+//String
+let val = 'hello';
+//boolean
+let b = true;
+//number
+let c = 90;
+//undefined
+let h = undefined;
+// bigInt
+let g = 1345345n;
+//symbol
+let s = Symbol('Hello');
 //# sourceMappingURL=index.js.map
--- typescript/day2/index.ts
+++ typescript/day2/index.ts
@@ -1,6 +1,36 @@
 //TYPE INFERENCE AND  ANNOTATION
-let a = 90;
+
+//Inference is the type of data passed to the variable first time it assign the data type same as data automatically
+
+// Where as Annotation is the Data type is defined by us.
+
+let a: number = 90;
 a = 20;
-a = 'pil';
 
 console.log(a);
+
+//Primitive Data Types
+
+//String
+
+let val: string = 'hello';
+
+//boolean
+
+let b: boolean = true;
+
+//number
+
+let c: number = 90;
+
+//undefined
+
+let h: undefined = undefined;
+
+// bigInt
+
+let g: bigint = 1345345n;
+
+//symbol
+
+let s: Symbol = Symbol('Hello');
