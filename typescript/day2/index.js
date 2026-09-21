a8c2eb116bb42af348356e862ead3fac01ee257a
Anuj Sisodiya <96922407+AnujSisodiya@users.noreply.github.com>
2026-09-22 04:40:37 +0530

    Array and Tuples

--- typescript/day2/index.js
+++ typescript/day2/index.js
@@ -19,4 +19,25 @@ let h = undefined;
 let g = 1345345n;
 //symbol
 let s = Symbol('Hello');
+// Arrays and Tuples
+//Array
+let aar = [4, 5, 6, 7, 'hello', 'jio', undefined, null, true];
+// here we also have to define which type of data should the array store
+//  Any -  means any type of data
+// Unknowm - Any type of data but it will cause problem while updating the data
+//Never - Nothing should come
+// let x: any = 'hello'; //It will not cause any type of error But we should not use it
+// let y: unknown = 'Array';
+// console.log(y.tOUpperCase());
+// Here there will be error of type unknown which will be throw my typescript bcoz it doest know the type of the variable and
+//we use it when we doesnt knew the type of data we will get from user or api
+let z;
+//Its usecase can be in When we delete and Insta post the variable will be assigned Never type
+//means it it will never store anything
+//Tuples
+let arr2 = [2, 55, true, 'hello'];
+let arr3 = [
+    { name: 'Anuj' },
+    { name: 'Riya' },
+];
 //# sourceMappingURL=index.js.map
